1e35b18779a2218b7d02bd8d955d633ab640332c
Kazuko Baynton <kazukobaynton@gmail.com>
2022-06-15 18:56:12 +0900

    refactor hooks

--- src/hooks/useApplicationData.js
+++ src/hooks/useApplicationData.js
@@ -1,87 +1,86 @@
-import {useEffect, useState} from 'react';
+import { useEffect, useState } from "react";
 import axios from "axios";
 import { getSpotsForDay } from "helpers/selectors";
 
-export default function useApplicationData () {
+export default function useApplicationData() {
   const [state, setState] = useState({
     day: "Monday",
     days: [],
     appointments: {},
-    interviewers: {}
+    interviewers: {},
   });
 
-  const setDay = day => setState({...state, day});
+  const setDay = (day) => setState({ ...state, day });
 
-  const updateSpots = function(state, appointments) {
+  const updateSpots = function (state, appointments) {
     // Get the day Object
-    const dayObj = state.days.find(day => day.name === state.day);
+    const dayObj = state.days.find((day) => day.name === state.day);
     const spots = getSpotsForDay(dayObj, appointments);
 
-    const day = {...dayObj, spots};
+    const day = { ...dayObj, spots };
 
-    const newDays = state.days.map(d => d.name === state.day ? day : d);
+    const newDays = state.days.map((d) => (d.name === state.day ? day : d));
 
     return newDays;
-  }
+  };
 
   useEffect(() => {
-    const daysURL=`http://localhost:8001/api/days`;
-    const appointmentsURL=`http://localhost:8001/api/appointments`;
-    const interviewersURL=`http://localhost:8001/api/interviewers`;
+    const daysURL = `http://localhost:8001/api/days`;
+    const appointmentsURL = `http://localhost:8001/api/appointments`;
+    const interviewersURL = `http://localhost:8001/api/interviewers`;
 
     Promise.all([
       axios.get(daysURL),
       axios.get(appointmentsURL),
-      axios.get(interviewersURL)
-    ]).then( response => {
-      // console.log(response[0]); first
-      // console.log(response[1]); second
-      // console.log(response[2]); third
-      setState(prev => ({...prev, days: response[0].data, appointments: response[1].data, interviewers: response[2].data }));
-    })
+      axios.get(interviewersURL),
+    ]).then((response) => {
+      setState((prev) => ({
+        ...prev,
+        days: response[0].data,
+        appointments: response[1].data,
+        interviewers: response[2].data,
+      }));
+    });
   }, []);
 
-
-  function bookInterview (id, interview) {
-    // console.log(id, interview);
+  function bookInterview(id, interview) {
     const appointment = {
       ...state.appointments[id],
-      interview: { ...interview }
+      interview: { ...interview },
     };
 
     const appointments = {
       ...state.appointments,
-      [id]: appointment
-    }
+      [id]: appointment,
+    };
 
     // Make the request to the app id ednpoints, with the interview data in the body,
-    return (
-      axios.put(`http://localhost:8001/api/appointments/${id}`, { interview })
-      .then( () => {
+    return axios
+      .put(`http://localhost:8001/api/appointments/${id}`, { interview })
+      .then(() => {
         const days = updateSpots(state, appointments);
-        setState({...state, appointments, days})})
-      );
+        setState({ ...state, appointments, days });
+      });
   }
 
-  function cancelInterview (id) {
-    // console.log("appointment id >>>>", id)
+  function cancelInterview(id) {
     const appointment = {
       ...state.appointments[id],
-      interview: null
+      interview: null,
     };
 
     const appointments = {
       ...state.appointments,
-      [id]: appointment
-    }
+      [id]: appointment,
+    };
 
-    return (
-      axios.delete(`http://localhost:8001/api/appointments/${id}`)
-      .then( () => {
+    return axios
+      .delete(`http://localhost:8001/api/appointments/${id}`)
+      .then(() => {
         const days = updateSpots(state, appointments);
-        setState({...state, appointments, days})})
-      );
+        setState({ ...state, appointments, days });
+      });
   }
 
-  return {state, setDay, bookInterview, cancelInterview}
+  return { state, setDay, bookInterview, cancelInterview };
 }
--- src/hooks/useVisualMode.js
+++ src/hooks/useVisualMode.js
@@ -1,31 +1,25 @@
-import {useState} from 'react';
+import { useState } from "react";
 
 export default function useVisualMode(initial) {
-
   // mode will always be the last element of history,
   // therefore you don't have to create mode state,
   // just return mode(last element of history array at the bottom)
 
-  // const [mode, setMode] = useState(initial);
   const [history, setHistory] = useState([initial]);
 
-  function transition (newMode, replace = false) {
-    // setMode(newMode);
-
-    if(replace) {
-      return setHistory(prev => [...prev.slice(0, -1), newMode]);
+  function transition(newMode, replace = false) {
+    if (replace) {
+      return setHistory((prev) => [...prev.slice(0, -1), newMode]);
     }
-    setHistory(prev => ([...prev, newMode]));
+    setHistory((prev) => [...prev, newMode]);
   }
 
-  function back () {
-    if(history.length > 1) {
-      setHistory(prev => ([...prev.slice(0, -1)]));
-      // const length = history.length
-      // setMode(history[length - 1]);
+  function back() {
+    if (history.length > 1) {
+      setHistory((prev) => [...prev.slice(0, -1)]);
     }
   }
 
   // returning the last history element as mode
-  return { mode:history[history.length - 1], transition, back };
-};
+  return { mode: history[history.length - 1], transition, back };
+}
